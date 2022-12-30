a488fa4ccf309c7cbdfc6151d40bed45f8b58309
BojanSibar <bojan.sibar@gmail.com>
2022-12-30 11:57:51 -0500

    add random style prefix

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,25 +1,28 @@
 import React from "react";
-import {Switch, Route, BrowserRouter, Router} from "react-router-dom";
-import {createGenerateClassName,StylesProvider } from "@material-ui/core/styles";
+import { Switch, Route, BrowserRouter, Router } from "react-router-dom";
+import {
+  createGenerateClassName,
+  StylesProvider,
+} from "@material-ui/core/styles";
 
 import Landing from "./components/Landing";
 import Pricing from "./components/Pricing";
 
-// const generateClassName = createGenerateClassName({
-//     productionPrefix: 'ma',
-//   });
+const generateClassName = createGenerateClassName({
+  productionPrefix: "ma",
+});
 
 export default () => {
-    return (
-        <div>
-          <StylesProvider>
-            <BrowserRouter>
-              <Switch>
-                <Route exact path="/pricing" component={Pricing} />
-                <Route path="/" component={Landing} />
-              </Switch>
-            </BrowserRouter>
-          </StylesProvider>
-        </div>
-      );
-}
+  return (
+    <div>
+      <StylesProvider generateClassName={generateClassName}>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/pricing" component={Pricing} />
+            <Route path="/" component={Landing} />
+          </Switch>
+        </BrowserRouter>
+      </StylesProvider>
+    </div>
+  );
+};
